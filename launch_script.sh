6bedd4e2397e68e324bd4743746932f750224d0c
tblatrille <tblatrille@uc.cl>
2021-03-19 11:17:43 +0100

    chore: update docker compose install script

--- launch_script.sh
+++ launch_script.sh
@@ -1,8 +1,8 @@
 # Export env variables
-# export VERSION=freeipa-server:centos-8
-# export HOSTNAME=XXXX
-# export DOMAIN=XXX
-# export PASSWORD=XXX
+export VERSION=freeipa-server:centos-8
+export HOSTNAME=freeipa.aws.sac.zone
+export DOMAIN=aws.sac.zone
+export PASSWORD=sacAgile!1492
 sudo curl -o lightsail-compose.sh https://raw.githubusercontent.com/tblatrille/freeipa-docker_compose/main/lightsail-compose.sh
 
 sudo chmod +x ./lightsail-compose.sh
--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -1,12 +1,22 @@
 # !/bin/bash
 
 # install latest version of docker the lazy way
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
 sudo curl -sSL https://get.docker.com | sh
 
 sudo usermod -aG docker ubuntu
 
 # install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 sudo chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker
